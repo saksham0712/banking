4121e4418c33d329c08fd60a7c05c004dd3d56dc
saksham0712 <sakshamverma626@gmail.com>
2024-11-07 03:16:13 +0530

    add react-chart

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,29 @@
+import HeaderBox from '@/components/HeaderBox'
+import TotalBalanceBox from '@/components/TotalBalanceBox'
 import React from 'react'
 
 const home = () => {
+  const loggedIn = { firstName: 'saksham'}
   return (
-    <div>
-      Dashboard
-    </div>
+
+    <section className='home'>
+      <div className='home-content'>
+        <header className='home-header'>
+          <HeaderBox
+            type="greeting"
+            title="welcome"
+            user={loggedIn?.firstName || 'Guest'}
+            subtext="Access and manage your account and transactions effocantly"
+          />
+
+          <TotalBalanceBox
+            accounts={[]}
+            totalBanks={1}
+            totalCurrentBalance={1250.35}
+          />
+        </header>
+      </div>
+    </section>
   )
 }
 
---
+++ components/DoughnutChart.tsx
@@ -0,0 +1,13 @@
+"use client"
+import React from 'react'
+import { Doughnut } from 'react-chartjs-2';
+
+
+const DoughnutChart = ({ accounts }: DoughnutChartProps) => {
+    
+    const data = {}
+  return <Doughnut data={[]} />
+
+}
+
+export default DoughnutChart
